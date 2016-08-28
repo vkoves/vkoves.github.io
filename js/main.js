e57a3fa455116538c3dc9a68e7c29f1c55532ddc
Viktor Koves <viktorkoves@gmail.com>
2016-08-28 01:19:38 -0500

    Moved Head Contents to JavaScript
    Now the <head> contents are also loaded in by main.js. This could pose
    issues for SEO, but I'd rather have one version and move it into each
    file later on when SEO is more important then simple active development.

--- js/main.js
+++ js/main.js
@@ -15,6 +15,25 @@ $(document).ready(function()
 		'</div>' +
 	'</div>');
 
+	$("head").append(
+	'<title>Viktor K&ouml;ves</title> <!-- Oh what fun, an Oumlaut! -->' +
+	'<link rel="shortcut icon" type="image/png" href="images/favicon.ico"/>' +
+	'<!-- Added Open Sans from Google Fonts -->' +
+	'<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,800italic,800,700,700italic,600italic,600,400italic" rel="stylesheet" type="text/css">' +
+	'<link href="css/main.css" rel="stylesheet">' +
+	'<meta name="theme-color" content="#D80000">' +
+	'<meta name="viewport" content="width=450, initial-scale=0.75">' +
+	'<meta name="google-site-verification" content="aivQX75RILmCefXvQj496cBxd8ycZ-AWTKRMQJGoyA4" />' +
+	'<!-- Google Analytics -->' +
+	"<script>" +
+	  "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" +
+	  "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," +
+	  "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" +
+	  "})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');" +
+	  "ga('create', 'UA-39148744-3', 'auto');" +
+	  "ga('send', 'pageview');" +
+	"</script>");
+
 	$("#menu").click(function()
 	{
 		var topPadding = parseInt($("#header").css("padding-bottom"));
