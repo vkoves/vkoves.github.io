10f12a1b9295636826e92d60187cbc399818d313
Viktor Koves <viktorkoves@gmail.com>
2016-08-28 01:04:20 -0500

    Moved Header into JavaScript
    Moved the header into main.js so that I don't have to update it on each
    page individually. Next I'll move the <head> tag contents in there as
    well.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function()
 {
+	//Add in the header. This makes it so I don't have to update the header in each file
+	$("body").prepend(
+	'<div id="header">' +
+		'<div id="header-inside">' +
+			'<span id="title"><a href="index.html">Viktor K&ouml;ves</a></span>' +
+			'<div id="header-buttons">' +
+				'<span><a href="about.html">About</a></span> |' +
+				'<span><a href="portfolio.html">Portfolio</a></span> |' +
+				'<span><a href="http://www.indigobox.us">IndigoBox</a></span> |' +
+				'<span><a href="lowvoltage.html">Low Voltage</a></span>' +
+			'</div>' +
+			'<img id="menu" src="images/menu.svg">' +
+		'</div>' +
+	'</div>');
+
 	$("#menu").click(function()
 	{
 		var topPadding = parseInt($("#header").css("padding-bottom"));
@@ -12,7 +27,7 @@ $(document).ready(function()
 		{
 			$("#header").css("padding-bottom", "30");
 			$("body").css("padding-top", "30");
-		}	
+		}
 	});
 });
 
