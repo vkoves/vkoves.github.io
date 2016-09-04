1a2a5ecf0ad0b0a72e82b4d15ae1f2a677faec99
Viktor Koves <viktorkoves@gmail.com>
2016-09-04 17:25:36 -0500

    Moved Gallery to Main Javascript
    Moved the gallery class to the main javascript file, and also fixed an
    issue with it not hiding controls, as well as an issue with it not
    showing info for images.

--- js/main.js
+++ js/main.js
@@ -130,3 +130,135 @@ function closeInfo()
 		$(this).remove();
 	});
 }
+
+// instantiates a gallery given a gallery data object, which contains info about all images to display
+// this can be either an array of hashes if you want more information in each gallery item
+// or an array of strings if you just need to display images
+// Possible options
+//	showInfo {Boolean} - whether to show a small info icon on the bottom left. DEFAULT - false
+function Gallery(galleryData, options)
+{
+	this.galleryData = galleryData; //the gallery data
+	this.currentImageIndex; //the index of the image being displayed in the gallery data
+	self = this; // we need a scoped version of the gallery object for reference in functions
+
+	// Create the HTML if it isnt' there already
+	if($("#overlay-main").length == 0)
+	{
+		$("body").append(
+			'<div id="overlay-main" class="overlay overlay-transparent">' +
+			'</div>' +
+			'<div id="overlay-controls" class="overlay">' +
+				'<img id="close" src="images/icons/cross.svg">' +
+				'<img id="right" class="vertically-centered arrow" src="images/icons/chevron-thin-right.svg">' +
+				'<img id="left" class="vertically-centered arrow" src="images/icons/chevron-thin-left.svg">' +
+			'</div>'
+		);
+	}
+
+	// Show the image with the given index in the galleryData
+	this.showImage = function(index)
+	{
+		self.currentImageIndex = index;
+		setOverlayImage(getImageUrl(this.galleryData[index])); //set the image
+		$(".overlay").fadeIn(); //then fade in
+	}
+
+	// Go to the next image
+	this.nextImage = function()
+	{
+		if($("#overlay-info:visible").length > 0) //if info is showing up
+			return; //don't do anything
+
+		if(self.currentImageIndex < self.galleryData.length - 1) // if we aren't on the last item in the gallery
+	  		self.currentImageIndex++; //go to next
+	  	else //otherwise, loop back to the first
+	  		self.currentImageIndex = 0;
+
+	  	self.showImage(self.currentImageIndex);
+	}
+	this.previousImage = function()
+	{
+		if($("#overlay-info:visible").length > 0) //if info is showing up
+			return;
+
+		if(self.currentImageIndex > 0)
+	  		self.currentImageIndex--;
+	  	else
+	  		self.currentImageIndex = self.galleryData.length - 1;
+
+	  	self.showImage(self.currentImageIndex);
+	}
+
+	// closes the gallery with a nice animation
+	this.close = function()
+	{
+		$(".overlay").fadeOut();
+	}
+
+	// Now that all methods are defined, let's map them to the DOM
+	$("#overlay-main, #overlay-controls #close").click(self.close);
+	$("#overlay-controls #right").click(self.nextImage);
+	$("#overlay-controls #left").click(self.previousImage);
+
+	/*****************************/
+	/****** PRIVATE METHODS ******/
+	/*****************************/
+
+	function setOverlayImage(url)
+	{
+		if($("#overlay-main:visible").length > 0 && $("#overlay-main img").length > 0) //if there's already an image
+		{
+			$("#overlay-main .img-container").fadeOut(300, function() //fade it out
+			{
+				//then transition to new image by setting it, hiding it instantly, then fadin in
+				setOverlayHTMLWithImage(url);
+			  	$("#overlay-main .img-container").hide().fadeIn(300);
+			});
+		}
+		else
+		{
+			setOverlayHTMLWithImage(url);
+		}
+
+		function setOverlayHTMLWithImage(url)
+		{
+		  	$("#overlay-main").html(
+		  	'<div class="img-base centered">' +
+		  		'<div class="img-container">' +
+			  		'<img src="' + url + '">' +
+			  		'<div class="icon info"></div>' +
+		  		'</div>' +
+		  	'</div>');
+
+		  	$("#overlay-main .img-container").click(function(event)
+		  	{
+		  		event.stopPropagation();
+		  	});
+
+		  	$("#overlay-main .img-container .icon.info").click(function()
+		  	{
+		  		showInfo(self.galleryData[self.currentImageIndex].title, self.galleryData[self.currentImageIndex].description)
+		  	});
+		}
+	}
+
+	/*
+	 * Returns the image url from a given gallery item. This is needed as gallery items
+	 * may be Javascript objects with a URL field, or just straight string URLs.
+	 * @param {Object/string} galleryItem - The gallery item we want to get the URL of
+	 * @return {string}	- The URL of the gallery item passed in
+	 */
+	function getImageUrl(galleryItem)
+	{
+		if (typeof galleryItem == "object")
+			return galleryItem.url;
+		else if (typeof galleryItem == "string")
+			return galleryItem;
+		else
+		{
+			console.error("Invalid gallery item type: " + typeof galleryItem);
+			return null;
+		}
+	}
+}
