c19c4e3b256a44fb8c179c050df9db094596db32
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2019-06-23 17:37:14 -0500

    Updated ci.sh to fix packback.co link error

--- ci.sh
+++ ci.sh
@@ -12,10 +12,10 @@ jekyll build
 # --assume-extension			- Fix needing the .html extension on links
 # --check_html					- Check for HTML errors using Nokogiri
 # --enforce_https				- Enforce HTTPS (since hosted on HTTPS)
-# --http-status-ignore "999"	- Ignore broken links with no error (caused by LinkedIn)
+# --http-status-ignore "999,0"	- Ignore broken links with no code 999 or 0 (caused by LinkedIn and Packback)
 #
 # Learn more: https://github.com/gjtorikian/html-proofer#configuration
-htmlproofer ./_site --allow-hash-href --assume-extension --check-html --http-status-ignore "999"
+htmlproofer ./_site --allow-hash-href --assume-extension --check-html --http-status-ignore "999,0"
 
 
 # Run Sass Lint verbose - cnfigured by .sass-lint.yml
