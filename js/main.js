6c39934d9d4d2422d8882078aafa79aa3debbd97
Viktor Koves <3187531+vkoves@users.noreply.github.com>
2020-01-31 21:30:52 -0600

    Added ESLint and fixed most issues

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function()
 {
-	$("#menu").click(function()
-	{
-		$("#header").toggleClass('-open');
-	});
+    $('#menu').click(function()
+    {
+        $('#header').toggleClass('-open');
+    });
 });
 
 /* General Functions */
@@ -11,64 +11,64 @@ $(document).ready(function()
 // Shows an overlay with text, including a title and description, to explain something.
 function showInfo(title, description)
 {
-	$("body").append('<div id="overlay-info" class="overlay-transparent">' +
-		'<div class="centered info-cont">' +
-			'<div class="title">' + title + '</div>' +
-			'<div class="description">' + description + '</div>' +
-		'</div>' +
-		'<button id="close" class="over-btn">' +
-			'<img alt="Close info overlay" src="images/icons/cross.svg">' +
-		'</button>' +
-	'</div>');
-	$("#overlay-info .centered").click(function(event)
-	{
-		event.stopPropagation();
-	});
-	$("#overlay-info").click(closeInfo);
-	$("#overlay-info").fadeIn();
-	setPageBlur(true);
+    $('body').append('<div id="overlay-info" class="overlay-transparent">' +
+        '<div class="centered info-cont">' +
+            '<div class="title">' + title + '</div>' +
+            '<div class="description">' + description + '</div>' +
+        '</div>' +
+        '<button id="close" class="over-btn">' +
+            '<img alt="Close info overlay" src="images/icons/cross.svg">' +
+        '</button>' +
+    '</div>');
+    $('#overlay-info .centered').click(function(event)
+    {
+        event.stopPropagation();
+    });
+    $('#overlay-info').click(closeInfo);
+    $('#overlay-info').fadeIn();
+    setPageBlur(true);
 }
 
 // Fades out and removes the info overlay specifically
 function closeInfo()
 {
-	setPageBlur(false);
-	$("#overlay-info").fadeOut(function()
-	{
-		$(this).remove();
-	});
+    setPageBlur(false);
+    $('#overlay-info').fadeOut(function()
+    {
+        $(this).remove();
+    });
 }
 
 // Blur the page and disable focus on it
 function setPageBlur(enableBlur)
 {
-	var elems = $("#header, .page-container, .footer");
-	var tabbableElements = $('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, *[tabindex], *[contenteditable]');
+    var elems = $('#header, .page-container, .footer');
+    var tabbableElements = $('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, *[tabindex], *[contenteditable]');
 
-	if (enableBlur) {
-		elems.addClass("blur");
-		disableTabbingOnPage(tabbableElements);
-	}
-	else {
-		elems.removeClass("blur");
-		reEnableTabbingOnPage(tabbableElements);
-	}
+    if (enableBlur) {
+        elems.addClass('blur');
+        disableTabbingOnPage(tabbableElements);
+    }
+    else {
+        elems.removeClass('blur');
+        reEnableTabbingOnPage(tabbableElements);
+    }
 }
 
 // Source: https://www.webappcessibility.com/technical/keeping-tab-focus-within-modals/
 function disableTabbingOnPage(tabbableElements) {
-	$.each(tabbableElements, function (index, elem) {
-		// Ensure not in main modal, info modal or modal controls
-		if($(elem).parents("#overlay-controls").length == 0
-			&& $(elem).parents("#overlay-main").length == 0
-			&& $(elem).parents("#overlay-info").length == 0) {
-			$(elem).attr('tabindex', '-1');
-		}
-	})
+    $.each(tabbableElements, function (index, elem) {
+        // Ensure not in main modal, info modal or modal controls
+        if($(elem).parents('#overlay-controls').length == 0
+            && $(elem).parents('#overlay-main').length == 0
+            && $(elem).parents('#overlay-info').length == 0) {
+            $(elem).attr('tabindex', '-1');
+        }
+    });
 }
 
 function reEnableTabbingOnPage(tabbableElements) {
-	$.each(tabbableElements, function (index, elem) {
-		$(elem).attr('tabindex', '0');
-	})
+    $.each(tabbableElements, function (index, elem) {
+        $(elem).attr('tabindex', '0');
+    });
 }
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,17 +1,16 @@
-var project; //the current project
 var galleryInstance;
 
 $(document).ready(function()
 {
-    $(".icons-cont .info-cont").click(function()
+    $('.icons-cont .info-cont').click(function()
     {
-        var project = getProject($(this).parent().attr("data-project-id"));
+        var project = getProject($(this).parent().attr('data-project-id'));
         showInfo(project.name, project.description);
     });
 
-    $(".icons-cont .images-cont").click(function()
+    $('.icons-cont .images-cont').click(function()
     {
-        var project = getProject($(this).parent().attr("data-project-id"));
+        var project = getProject($(this).parent().attr('data-project-id'));
 
         if (galleryInstance) {
             galleryInstance.destroy();
@@ -21,35 +20,35 @@ $(document).ready(function()
         galleryInstance.showImage(0); //and show the first image
     });
 
-    $(".icons-cont .tools-cont").click(function()
+    $('.icons-cont .tools-cont').click(function()
     {
-        var project = getProject($(this).parent().attr("data-project-id"));
+        var project = getProject($(this).parent().attr('data-project-id'));
 
-        showInfo(project.name + " - Tools", getToolsHTML(project.tools));
+        showInfo(project.name + ' - Tools', getToolsHTML(project.tools));
     });
 
-    $("body").keydown(function(event)
+    $('body').keydown(function(event)
     {
-      if (event.keyCode == 37) // left arrow
-      {
-        galleryInstance.previousImage();
-      }
-      else if (event.keyCode == 39) // right arrow
-      {
-        galleryInstance.nextImage();
-      }
-      else if (event.keyCode == 27) // escape
-      {
-        if (galleryInstance)
+        if (event.keyCode == 37) // left arrow
         {
-            galleryInstance.destroy();
-            galleryInstance = null;
+            galleryInstance.previousImage();
         }
-        else
+        else if (event.keyCode == 39) // right arrow
         {
-            closeInfo();
+            galleryInstance.nextImage();
         }
-      }
+        else if (event.keyCode == 27) // escape
+        {
+            if (galleryInstance)
+            {
+                galleryInstance.destroy();
+                galleryInstance = null;
+            }
+            else
+            {
+                closeInfo();
+            }
+        }
     });
 });
 
@@ -65,7 +64,7 @@ function getProject(projectId) {
 
 function getToolsHTML(tools)
 {
-    var toolsHTML = ""; // start generating HTML for the tools to display
+    var toolsHTML = ''; // start generating HTML for the tools to display
     var tool; // the current tool
     var toolImage = null;
 
@@ -78,11 +77,11 @@ function getToolsHTML(tools)
             toolsHTML += '<div class="tool-cont"><img src="' + toolImage + '"><div class="text">' + tool + '</div></div>';
         else
         {
-            var className = "featured-text"
+            var className = 'featured-text';
 
             // if it's a short abbreviation (e.g. C#, JS, CSS)
             if (tool.length < 3) {
-                className += " large"
+                className += ' large';
             }
 
             toolsHTML += '<div class="tool-cont"><div class="' + className + '">' + tool + '</div><div class="text">' + tool + '</div></div>';
@@ -94,10 +93,10 @@ function getToolsHTML(tools)
 
 function getToolsImage(toolName)
 {
-    if (toolName == "C#" || toolName == "Google Charts") { // no images for C# or Google Charts
+    if (toolName == 'C#' || toolName == 'Google Charts') { // no images for C# or Google Charts
         return null;
     }
     else {
-        return "images/tools/" + toolName.split(" ").join("_") + ".svg";
+        return 'images/tools/' + toolName.split(' ').join('_') + '.svg';
     }
 }
