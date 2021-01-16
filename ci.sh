7c6387508e02681c73716f9b44c0790a7a058e3e
Viktor Koves <3187531+vkoves@users.noreply.github.com>
2021-01-16 16:07:22 -0600

    Updated homepage copy and made it more minimal Also fixed CI failing due to broken link and Twitter error

--- ci.sh
+++ ci.sh
@@ -12,11 +12,11 @@ jekyll build
 # --assume-extension			- Fix needing the .html extension on links
 # --check_html					- Check for HTML errors using Nokogiri
 # --enforce_https				- Enforce HTTPS (since hosted on HTTPS)
-# --http-status-ignore "999,0"	- Ignore broken links with no code 999 or 0 (caused by LinkedIn and Packback)
+# --http-status-ignore "999,400,0"	- Ignore broken links with code 999, 400 or 0 (caused by LinkedIn, Twitter, and Packback)
 # --empty_alt_ignore    - Allow empty alt tags for decorative images
 #
 # Learn more: https://github.com/gjtorikian/html-proofer#configuration
-htmlproofer ./_site --allow-hash-href --assume-extension --check-html --http-status-ignore "999,0" --empty_alt_ignore --url-ignore "/codepen.io/"
+htmlproofer ./_site --allow-hash-href --assume-extension --check-html --http-status-ignore "999,400,0" --empty_alt_ignore --url-ignore "/codepen.io/"
 
 
 # Run Sass Lint verbose - cnfigured by .sass-lint.yml
